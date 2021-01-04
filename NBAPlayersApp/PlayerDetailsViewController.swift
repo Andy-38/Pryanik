4b8c6c8555438aa33653c9aa39f6a04acef32e33
Andrey Dvoytsov <annihilator1981@mail.ru>
2021-01-04 17:39:04 +0500

    Add second screen

---
+++ NBAPlayersApp/PlayerDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PlayerDetailsViewController.swift
+//  NBAPlayersApp
+//
+//  Created by Андрей Двойцов on 04.01.2021.
+//
+
+import UIKit
+
+class PlayerDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.title = "Players"
+        navigationController?.navigationBar.prefersLargeTitles = true // большие заголовки
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- NBAPlayersApp/PlayersViewController.swift
+++ NBAPlayersApp/PlayersViewController.swift
@@ -7,23 +7,33 @@
 
 import UIKit
 
-class PlayersViewController: UIViewController {
+class PlayersViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationItem.title = "Players"
+        navigationController?.navigationBar.prefersLargeTitles = true // большие заголовки
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+        // сколько раз вызывается функция следующая
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlayerCell", for: indexPath)
+        cell.textLabel?.text = "Player \(indexPath.row)"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        let playerDetailsViewController = storyboard.instantiateViewController(identifier: "PlayerDetails")
+        
+        navigationController?.pushViewController(playerDetailsViewController, animated: true)
+    }
+    
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
